f580b8bd8cbd4eac4b8ccca25b422d02080a08b6
Vasyl Dubno <vasyldubno@gmail.com>
2023-02-12 14:20:07 -0800

    init

--- controllers/CreateCart.js
+++ controllers/CreateCart.js
@@ -2,6 +2,8 @@ import { Cart } from '../models/Cart.js'
 import { v4 } from 'uuid'
 
 export const CreateCart = async (req, res) => {
-  const cart = await Cart.create({})
-  res.json({ cart })
+  try {
+    const cart = await Cart.create({})
+    res.json({ cart })
+  } catch {}
 } 
