1276d5bb11770ad79873cb3bf1e5cab8682d41c1
Vasyl Dubno <vasyldubno@gmail.com>
2023-08-31 16:52:48 -0700

    fix

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({ 
 	origin: ['https://electronshop.vercel.app/'],
+	credentials: true
 }))
 app.use(router)
 
